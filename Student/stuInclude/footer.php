bdb56b5463f768e78ad0360f3460ff2d119ccb18
akanshagupta12 <akansha.gupta_cs18@gla.ac.in>
2021-04-17 21:27:03 +0530

    commit

--- Student/stuInclude/footer.php
+++ Student/stuInclude/footer.php
@@ -1,4 +1,4 @@
-    
+    <!-- Footer -->
     <!-- Jquery and Boostrap JavaScript -->
     <script type="text/javascript" src="../js/jquery.min.js"></script>
     <script type="text/javascript" src="../js/popper.min.js"></script>
